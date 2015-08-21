02940dffdf326bdd96719ccb37ee167e94709eab
couchbasebrian <brian.williams@couchbase.com>
2015-08-21 14:07:35 -0700

    cleanup - renaming

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+# August 21, 2015
+#
+clear
+# Assumes a git pull for libcouchbase was done in the current directory
 LIBS=./libcouchbase/build/lib
-rm -rf ./hello
-gcc -I./libcouchbase/build/generated -I./libcouchbase/include -I./libcouchbase/include/libcouchbase -L$LIBS  hello.c -lcouchbase -o hello
-if [ -r ./hello ]
+#
+echo "---> Removing old executable file"
+rm -rf ./lcbtesting
+#
+echo "---> Invoking gcc"
+gcc -I./libcouchbase/build/generated -I./libcouchbase/include -I./libcouchbase/include/libcouchbase -L$LIBS  lcbtesting.c -lcouchbase -o lcbtesting
+#
+if [ -r ./lcbtesting ]
 then
-    ls -l ./hello
+    echo "---> This is the newly built executable"
+    ls -l ./lcbtesting
     export LD_LIBRARY_PATH=./libcouchbase/build/lib
-    ./hello
+    echo "---> Running the executable:"
+    ./lcbtesting
+    echo "---> Done running the executable."
 else
-    echo "There is no hello"
+    echo "---> Failed to build the executable"
 fi
+#
+echo "---> Goodbye"
+exit 0
+
